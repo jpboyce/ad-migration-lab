1b5f222e93aa2ce86837f874b7bfcdd1af99c289
Jesse Boyce <jesse@jpboyce.org>
2018-10-18 01:47:59 +1000

    Added subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,6 +39,45 @@ resource "aws_internet_gateway" "default" {
   vpc_id = "${aws_vpc.adlab.id}"
 }
 
+
+# Subnets
+
+# Domain1 Public Subnet
+resource "aws_subnet" "domain1_subnet_public" {
+  vpc_id = "${aws_vpc.adlab.id}"
+  cidr_block = "${var.domain1_cidr_public}"
+  tags {
+    ResourceGroup = "${var.tag_text_default}"
+  }
+}
+# Domain1 Private Subnet
+resource "aws_subnet" "domain1_subnet_private" {
+  vpc_id = "${aws_vpc.adlab.id}"
+  cidr_block = "${var.domain1_cidr_private}"
+  tags {
+    ResourceGroup = "${var.tag_text_default}"
+  }
+}
+
+# Domain2 Public Subnet
+resource "aws_subnet" "domain2_subnet_public" {
+  vpc_id = "${aws_vpc.adlab.id}"
+  cidr_block = "${var.domain2_cidr_public}"
+  tags {
+    ResourceGroup = "${var.tag_text_default}"
+  }
+}
+# Domain2 Private Subnet
+resource "aws_subnet" "domain2_subnet_private" {
+  vpc_id = "${aws_vpc.adlab.id}"
+  cidr_block = "${var.domain2_cidr_private}"
+  tags {
+    ResourceGroup = "${var.tag_text_default}"
+  }
+}
+
+
+
 # Create a subnet
 resource "aws_subnet" "default_subnet" {
   vpc_id = "${aws_vpc.adlab.id}"
