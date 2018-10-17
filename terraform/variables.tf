a012ff402c7179d979af5541c9769efe1803162c
Jesse Boyce <jesse@jpboyce.org>
2018-10-18 01:08:20 +1000

    Added AMI map

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,13 @@ variable "cidr_block_default" {
   description = "Default CIDR block value"
   default = "10.0.0.0/16"
 }
+
+variable "aws_amis" {
+  type = "map"
+  description = "AWS AMI Mappings for OSes"
+  default = {
+    "windows2008sp2" = "ami-7d35d31f"
+    "windows2008r2sp1" = "ami-29cd2a4b"
+    "windows2012r2" = "ami-ed33d48f"
+  }
+}
